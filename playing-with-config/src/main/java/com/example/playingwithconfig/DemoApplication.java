8455378ed6eb3b9638b7b676d9d7232d79ac32df
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-08-30 08:33:00 +0530

    use two custom configs

--- playing-with-config/src/main/java/com/example/playingwithconfig/DemoApplication.java
+++ playing-with-config/src/main/java/com/example/playingwithconfig/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.playingwithconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @Value("${customConfig}")
+    String customConfig;
 
+    @Value("${anotherCustomConfig}")
+    String anotherCustomConfig;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping(path = "/")
+    public String hello() {
+        return customConfig + ", " + anotherCustomConfig;
+    }
 }
--- playing-with-config/src/test/java/com/example/playingwithconfig/DemoApplicationTests.java
+++ playing-with-config/src/test/java/com/example/playingwithconfig/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.playingwithconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Value("${customConfig}")
+	String customConfig;
+
+	@Value("${anotherCustomConfig}")
+	String anotherCustomConfig;
+
 	@Test
 	void contextLoads() {
+		assertEquals("someValue", customConfig);
+		assertEquals("anotherValue", anotherCustomConfig);
 	}
 
 }
