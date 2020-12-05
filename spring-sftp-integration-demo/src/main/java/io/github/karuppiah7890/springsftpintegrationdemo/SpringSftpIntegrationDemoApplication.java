8c3d8f32ca23be6635ca0835deea046dc1d975a7
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-12-05 12:41:51 +0530

    boostrap spring sftp integration demo app

---
+++ spring-sftp-integration-demo/src/main/java/io/github/karuppiah7890/springsftpintegrationdemo/SpringSftpIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.karuppiah7890.springsftpintegrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSftpIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSftpIntegrationDemoApplication.class, args);
+	}
+
+}
